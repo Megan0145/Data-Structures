645944fecc35d8646284194f0773daab2b9b6141
Megan Ennis <Megan@Megans-iMac-9.local>
2020-02-25 16:11:05 +0000

    Add LRU cache & notes

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,4 @@
+from doubly_linked_list import DoublyLinkedList
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -7,7 +8,10 @@ class LRUCache:
     to every node stored in the cache.
     """
     def __init__(self, limit=10):
-        pass
+        self.limit = limit
+        self.current_size = 0
+        self.entries = DoublyLinkedList()
+        self.storage = dict()
 
     """
     Retrieves the value associated with the given key. Also
@@ -17,7 +21,14 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        pass
+        if key not in self.storage:
+            return None
+
+        # move the node stored at the given key in our dictionary to the front (head) of the doubly linked list (making it the MOST recently accessed node)
+        self.entries.move_to_front(self.storage[key])  
+        # return the value of the node at the given key in our dictionary 
+        # could also return self.entries.head.value since we have just moved the node to the front of our DLL 
+        return self.storage[key].value
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -30,4 +41,32 @@ class LRUCache:
     the newly-specified value.
     """
     def set(self, key, value):
-        pass
+        # if the key already exists in storage
+        if key in self.storage:
+            # move the node stored at the given key in storage to the front (head) of the doubly linked list (making it the MOST recently used node)
+            self.entries.move_to_front(self.storage[key])  
+            # overwrite the value the node contains with value passed into 'set' method 
+            # (could also directly set value of head node in DLL equal to value passed into function, seeing as we've just moved the node to the head of the DLL)
+            self.storage[key].value = value
+
+        # if the cache is at max capacity (current_size is equal to the limit)
+        elif self.current_size == self.limit:
+           # remove the node at the tail of the DLL (this will be the Least recently used node), 
+           # and save the value of the node we've just removed from the tail to variable
+           removed_node = self.entries.remove_from_tail()
+           # update storage to only hold key value pairs where the value of the node stored at any key is not equal to the value of the node we have just removed from the tail of the DLL
+           self.storage = { k:v for k, v in self.storage.items() if v.value != removed_node }
+           # add a new node to the head of the DLL containing the value we are passing into the 'set' function, making it the MOST recently used
+           self.entries.add_to_head(value)
+           # add new key value pair to storage where key = key and value = the node we've just added to the head of the DLL 
+           self.storage[key] = self.entries.head
+
+        # else the key must not already exist in storage AND we are not at max capacity
+        else:
+            # add a new node to the head of the DLL containing the value we are passing into the 'set' function, making it the MOST recently used
+            self.entries.add_to_head(value)  
+            # add new key value pair to storage where key = key and value = the node we've just added to the head of the DLL  
+            self.storage[key] = self.entries.head
+            # increment the current size of storage by one because we have just added a new key value pair
+            self.current_size += 1
+
