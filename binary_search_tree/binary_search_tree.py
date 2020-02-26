e0e1234bb1fb8c996fe356fa6ec88efb0b89d6bb
Megan Ennis <Megan@Megans-iMac-9.local>
2020-02-26 13:01:53 +0000

    Add insert method to BST

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('../queue_and_stack')
-from dll_queue import Queue
-from dll_stack import Stack
+# from dll_queue import Queue
+# from dll_stack import Stack
 
 
 class BinarySearchTree:
@@ -12,8 +12,28 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        # check if new value will fall on RHS of value of current BST (is it MORE than OR EQUAL to the root node of the current BST)
+        if value >= self.value:
+            # if current BST does not have a right node
+            if not self.right:
+                # add new subtree to the right 
+                self.right = BinarySearchTree(value)
+                # return from function
+                return
+            # else recur insert function, this time passing the right node of the current node as the root node (ie self now == self.right)
+            return self.right.insert(value)
 
+        # else new value must fall on LHS of BST (it's LESS than value of the current BST)
+        else:
+            # if current BST does not have a left node
+            if not self.left:
+                # add new subtree to the left
+                self.left = BinarySearchTree(value)
+                # return from function
+                return
+            # else recur insert function, this time passing the left node of the current BST as the root node (ie self now == self.left)
+            return self.left.insert(value) 
+    
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
